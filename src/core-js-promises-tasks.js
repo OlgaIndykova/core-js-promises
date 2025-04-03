a0f79050a8b1c0ad8db5437c896e4bc58d615003
OlgaIndykova <ola1234567890dd@gmail.com>
2025-04-04 00:15:32 +0300

    feat: resolve getPromise, getFirstResolvedPromiseResult

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -17,8 +17,13 @@
  * 0    => promise that will be fulfilled
  * 1    => promise that will be fulfilled
  */
-function getPromise(/* number */) {
-  throw new Error('Not implemented');
+function getPromise(number) {
+  const promise = new Promise((resolve, reject) => {
+    if (number < 0) {
+      reject(new Error('rejected'));
+    } else resolve(number);
+  });
+  return promise;
 }
 
 /**
@@ -50,8 +55,8 @@ function getPromiseResult(/* source */) {
  * [Promise.resolve(1), Promise.reject(2), Promise.resolve(3)]  => Promise fulfilled with 1
  * [Promise.reject(1), Promise.reject(2), Promise.reject(3)]    => Promise rejected
  */
-function getFirstResolvedPromiseResult(/* promises */) {
-  throw new Error('Not implemented');
+function getFirstResolvedPromiseResult(promises) {
+  return Promise.any(promises);
 }
 
 /**
